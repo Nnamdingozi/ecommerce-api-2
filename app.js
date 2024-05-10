597684d2036256c91d7f9f4881500920e29c1a09
joerald <ngozika1105@gmail.com>
2024-05-10 08:17:11 +0100

    change entry point fron index.js to app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+app.get('/', (req, res) => {
+    res.send('E-commerce API')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on port: ${PORT}`)
+});
+
